3102d8c7a873b47bb4d6367650a0af4ca65fb36f
unknown <kaithwas.697@gmail.com>
2022-02-14 19:57:46 +0530

    Added daily wage feature

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,8 +1,13 @@
+ratePrHr=20
 isPresent=1
 check=$((RANDOM%2))
 if [ $check -eq $isPresent ]
-then 
+then
 	echo "Employee is Present"
+	wrkHr=8
 else
 	echo "Employee is absent"
+	wrkHr=0
 fi
+salary=$(($ratePrHr*wrkHr))
+echo "Daily salary is $salary"
