d072081716ff9a356c9271e8a545e9e0e84f8bc6
unknown <kaithwas.697@gmail.com>
2022-02-15 10:56:15 +0530

    Used switch case to find Employee wage

--- employeeWage.sh
+++ employeeWage.sh
@@ -2,24 +2,29 @@ ratePrHr=20
 isPresent=1
 partTime=$((RANDOM%2))
 check=$((RANDOM%2))
-if [ $check -eq $isPresent ]
-then
-	echo "Employee is Present"
-	wrkHr=8
-else
-	echo "Employee is absent"
-	wrkHr=0
-fi
+case $check in
+
+	$isPresent) echo "Employee is Present"
+		wrkHr=8
+		;;
+
+	*) echo "Employee is absent"
+		wrkHr=0
+		;;
+esac
 salary=$(($ratePrHr*wrkHr))
 echo "Daily salary is $salary"
 
-# Part time employee wage 
-if [ $partTime -eq $isPresent ]
-then
-	partTimeHr=8
-else
-	echo "Employee is absent"
-	partTimeHr=0
-fi
+# Part time employee wage
+case $check in
+
+	$partTime) echo "Employee is present"
+		partTimeHr=8
+		;;
+
+	*) echo "Employee is absent"
+		partTimeHr=0
+		;;
+esac
 salaryPartTime=$(($ratePrHr*partTimeHr))
 echo "Salary of part Time employee is $salaryPartTime"
