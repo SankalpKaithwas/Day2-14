e044cf8d2899bbd9a7a53669eac3a3834627a4d7
unknown <kaithwas.697@gmail.com>
2022-02-15 12:59:07 +0530

    Added Employee wage until a condition is reached

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,8 +1,10 @@
-ratePrDay=160
+ratePrHr=20
+workHr=0
+partWorkHr=0
 i=0
 days=0
 partDays=0
-while (($i<20))
+while (( $i<20 && $workHr<100 ))
 do
 check=$((RANDOM%2))
 isPresent=1
@@ -10,20 +12,22 @@ partTime=$((RANDOM%2))
 case $check in
 
 	$isPresent) days=$(($days+1))
+		workHr=$(($workHr+8))
 		;;
 esac
 case $check in
 
 	$partTime) partDays=$(($partDays+1))
+		partWorkHr=$(($partWorkHr+8))
 		;;
 esac
 
 i=$(($i+1))
 done
 
-salary=$(($days*$ratePrDay))
-echo "Monthly salary of employee is $salary"
+salary=$(($days*$ratePrHr*$workHr))
+echo "Monthly salary of employee is $salary for $days days $workHr hours"
 
 # Part time employee wage
-salaryPartTime=$(($partDays*$ratePrDay))
-echo "Monthly Salary of part Time employee is $salaryPartTime"
+salaryPartTime=$(($partDays*$ratePrHr*$partWorkHr))
+echo "Monthly Salary of part Time employee is $salaryPartTime for $partDays days $partWorkHr hours"
