89e17b868f0b15f26818823642e00ed60a68cb5b
unknown <kaithwas.697@gmail.com>
2022-02-15 10:22:39 +0530

    Added Part time employee wage feature

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,5 +1,6 @@
 ratePrHr=20
 isPresent=1
+partTime=$((RANDOM%2))
 check=$((RANDOM%2))
 if [ $check -eq $isPresent ]
 then
@@ -11,3 +12,14 @@ else
 fi
 salary=$(($ratePrHr*wrkHr))
 echo "Daily salary is $salary"
+
+# Part time employee wage 
+if [ $partTime -eq $isPresent ]
+then
+	partTimeHr=8
+else
+	echo "Employee is absent"
+	partTimeHr=0
+fi
+salaryPartTime=$(($ratePrHr*partTimeHr))
+echo "Salary of part Time employee is $salaryPartTime"
